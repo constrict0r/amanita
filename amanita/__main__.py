32738c5a7f80e1cd78cf517f8ba0d0674319cd21
constrict0r <constrict0r@protonmail.com>
2018-11-04 11:10:05 -0600

    Added cli file

--- amanita/__main__.py
+++ amanita/__main__.py
@@ -2,32 +2,7 @@
 
 """Main amanita CLI."""
 
-import os
-import sys
-
-import click
-
-from amanita import __version__
-
-
-def version_msg():
-    """Return the amanita version, location and Python powering it."""
-    python_version = sys.version[:3]
-    location = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    message = u'amanita %(version)s from {} (Python {})'
-    return message.format(location, python_version)
-
-
-@click.command(context_settings=dict(help_option_names=[u'-h', u'--help']))
-@click.version_option(__version__, u'-V', u'--version', message=version_msg())
-def main():
-    """amanita cli main rutine"""
-    # if args is None:
-    #     if len(sys.argv) > 1:
-    #         args = sys.argv[1]
-
-    print("This is the main routine.")
-
+from .cli import main
 
 if __name__ == "__main__":
-    sys.exit(main())
+    main()
---
+++ amanita/cli.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+"""Main amanita CLI."""
+
+import os
+import sys
+
+import click
+
+from amanita import __version__
+
+
+def version_msg():
+    """Return the amanita version, location and Python powering it."""
+    python_version = sys.version[:3]
+    location = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    message = u'amanita %(version)s from {} (Python {})'
+    return message.format(location, python_version)
+
+
+@click.command(context_settings=dict(help_option_names=[u'-h', u'--help']))
+@click.version_option(__version__, u'-V', u'--version', message=version_msg())
+# @click.option(
+#     u'-p',
+#     help=u'Path to project destination.',
+#     default='myNewProject',
+# )
+def main():
+    """amanita cli main rutine"""
+    # if args is None:
+    #     if len(sys.argv) > 1:
+    #         args = sys.argv[1]
+
+    print("This is the main routine.")
+    print("")
